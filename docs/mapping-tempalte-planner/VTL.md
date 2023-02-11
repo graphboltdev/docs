da44e12097a7577a22b90abbbc95e735e3188646
bboure <benoit.boure@gmail.com>
2023-02-11 17:52:40 +0100

    Initial commit

---
+++ docs/mapping-tempalte-planner/VTL.md
@@ -0,0 +1,5 @@
+---
+sidebar_position: 2
+---
+
+# VTL mapping templates
---
+++ docusaurus.config.js
@@ -0,0 +1,116 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'GraphBolt docs',
+  tagline: '',
+  favicon: 'img/favicon.png',
+
+  url: 'https://docs.graphbolt.dev',
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'graphboltdev', // Usually your GitHub org/user name.
+  projectName: 'graphbolt', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/graphboltdev/docs/edit/main/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/og-image.png',
+      navbar: {
+        logo: {
+          alt: 'GraphBolt',
+          src: 'img/logo-full-black.svg',
+          srcDark: 'img/logo-full.svg',
+        },
+        items: [
+          {
+            href: 'https://graphbolt.dev',
+            label: 'Download',
+            position: 'left'
+          },
+          {
+            href: 'https://blog.graphbolt.dev',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.gg/vekQvYHjPm',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/graphbolt',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                href: 'https://blog.graphbolt.dev',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/grpahboltdev/graphbolt',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} GraphBolt. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
